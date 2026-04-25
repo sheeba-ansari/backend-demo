98dd217dd9e4e63fc64ee34b1c7ed0824a982e38
sheeba-ansari <ansarisheeba5000@gmail.com>
2026-04-25 21:31:55 +0530

    update secret route

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 
+// Use environment variable for PORT
 const PORT = process.env.PORT || 3000;
+
+// Example environment variable (dummy)
 const SECRET = process.env.JWT_SECRET || "defaultsecret";
 
 app.get("/", (req, res) => {
-  res.send("Backend is working");
+  res.send("Backend is running with env variables!");
 });
 
 app.get("/secret", (req, res) => {
